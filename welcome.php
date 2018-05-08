cc523158144119436285a812d73f9796428d6f69
oflavor <37283050+oflavor@users.noreply.github.com>
2018-05-07 22:49:19 -0600

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+<nav>
+        <a class="button" href="index.html">Home</a>
+    </nav>
+</body>
+</html> 
